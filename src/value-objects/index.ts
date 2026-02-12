730ef62607763e14a2d8afb342ad5b6101d05f3e
Alan Reis <alanreisanjo@gmail.com>
2026-02-12 14:31:24 -0300

    feat: export DateTimeVO from value-objects module

--- src/value-objects/index.ts
+++ src/value-objects/index.ts
@@ -4,3 +4,4 @@ export { StringArrayVO } from "./string-array.value-object";
 export { UrlVO } from "./url.value-object";
 export { UuidArrayVO } from "./uuid-array.value-object";
 export { SlugVO } from "./slug.value-object";
+export { DateTimeVO } from "./date-time.value-object";
